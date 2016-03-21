e6f55c71e8bc552de9458db6670f87759b352acb
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-03-21 15:17:31 -0600

    updated to site

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,12 +1,6 @@
+// Local (client-only) collection
 Errors = new Mongo.Collection(null);
 
 throwError = function(message) {
-    Errors.insert({message: message});
-};
-
-Template.error.onRendered(function() {
-    var error = this.data;
-    Meteor.setTimeout(function () {
-        Errors.remove(error._id);
-    }, 3000);
-});
+  Errors.insert({message: message})
+}
