2f01c5a3ad2710ead7c90a63f64e440dce34d1c6
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-03-20 20:50:56 -0600

    updated errors

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -3,3 +3,10 @@ Errors = new Mongo.Collection(null);
 throwError = function(message) {
     Errors.insert({message: message});
 };
+
+Template.error.onRendered(function() {
+    var error = this.data;
+    Meteor.setTimeout(function () {
+        Errors.remove(error._id);
+    }, 3000);
+});
